d97558ae81ad5b56e02abbaa556b2f6e2080e7c9
PokingTeemo <110599863+PokingTeemo@users.noreply.github.com>
2023-11-26 15:18:59 +0900

    드디어 화면전환 가능
    마침내..마침내..

--- app/src/main/java/com/example/test2/Frag1.java
+++ app/src/main/java/com/example/test2/Frag1.java
@@ -1,20 +1,27 @@
 package com.example.test2;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.viewmodel.CreationExtras;
 
 public class Frag1 extends Fragment {
     private View view;
 
     @Nullable
-    @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag1,container,false);
         return view;
+    }
+
+    @NonNull
+    @Override
+    public CreationExtras getDefaultViewModelCreationExtras() {
+        return super.getDefaultViewModelCreationExtras();
     }
 }
--- app/src/main/java/com/example/test2/Frag3.java
+++ app/src/main/java/com/example/test2/Frag3.java
@@ -1,12 +1,14 @@
 package com.example.test2;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.viewmodel.CreationExtras;
 
 public class Frag3 extends Fragment {
     private View view;
@@ -16,5 +18,11 @@ public class Frag3 extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag3,container,false);
         return view;
+    }
+
+    @NonNull
+    @Override
+    public CreationExtras getDefaultViewModelCreationExtras() {
+        return super.getDefaultViewModelCreationExtras();
     }
 }
